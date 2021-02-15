cfea79c604ddae78522c1617d3e91de167736473
NICOLA\Grifon <grifonik21@gmail.com>
2021-02-15 23:32:13 +0100

    cleaning code

--- src/components/progressbar/index.jsx
+++ src/components/progressbar/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import stylesIn from './styles.module.scss';
